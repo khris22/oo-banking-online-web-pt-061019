db6e6251fb02d2c8ab2a4b1b132e9affbb793d12
Khris Punzalan <khrispunzalan@gmail.com>
2019-07-08 11:59:58 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -20,7 +20,7 @@ class Transfer
         binding.pry
         @status = "complete"
     else
-    
+
       "Transaction rejected. Please check your account balance."
       @status = "rejected"
     end
