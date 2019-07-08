ff1616f21994d11e956b5831e42534efd6814e8c
Khris Punzalan <khrispunzalan@gmail.com>
2019-07-08 11:49:41 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,7 +17,7 @@ class Transfer
     if valid? && sender.balance > amount && self.status == "pending"
         sender.balance -= amount
         receiver.balance += amount
-        self.status = "complete"
+        status = "complete"
     else
       p "Transaction rejected. Please check your account balance."
       self.status = "rejected"
