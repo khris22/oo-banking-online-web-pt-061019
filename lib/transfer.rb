b923db5770196b2cc83d5b979ee752ff1b0049a6
Khris Punzalan <khrispunzalan@gmail.com>
2019-07-08 12:22:19 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,7 +14,7 @@ class Transfer
   end
 
   def execute_transaction
-    if valid? && sender.balance > amount && @status == "pending"
+    if valid? && @status == "pending"
         sender.balance -= amount
         receiver.balance += amount
         # binding.pry
